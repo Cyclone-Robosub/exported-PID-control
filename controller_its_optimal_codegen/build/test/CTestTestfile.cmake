ac007c89fc88d65205fa489a397fa9fd7f65b8bf
zenyn <zenyn.ethridge@gmail.com>
2025-05-15 16:58:25 -0700

    fixed cmake

--- controller_its_optimal_codegen/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zenyn/exported-PID-control/controller_its_optimal_codegen/test
-# Build directory: /home/zenyn/exported-PID-control/controller_its_optimal_codegen/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
